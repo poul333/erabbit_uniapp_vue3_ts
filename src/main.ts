35dc3692405eef75dddc625fe7d88ce14f9e464b
poul <2978688528@qq.com>
2023-01-18 22:38:26 +0800

    23-01-18 项目初始化

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { createSSRApp } from 'vue'
+import App from './App.vue'
+
+import { createPinia } from 'pinia'
+
+export function createApp() {
+  const app = createSSRApp(App)
+
+  app.use(createPinia())
+
+  return {
+    app,
+  }
+}
